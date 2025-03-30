41e66fb853ff0ec3abcb6104bf83f0b4a2c64d89
Baby Bear <132609968+smallbabybear@users.noreply.github.com>
2025-03-30 12:20:38 +0700

    Fix PredicatePermitter

--- contracts/permitter/PredicatePermitter.sol
+++ contracts/permitter/PredicatePermitter.sol
@@ -4,59 +4,72 @@ pragma solidity 0.8.27;
 import {IPermitter} from "./IPermitter.sol";
 import {PredicateClient} from "@predicate/contracts/src/mixins/PredicateClient.sol";
 import {PredicateMessage} from "@predicate/contracts/src/interfaces/IPredicateClient.sol";
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
 /**
  * @title PredicatePermitter
  * @notice Implementation of IPermitter that uses Predicate Protocol for permission validation
- * @dev Non-upgradeable version that leverages Predicate Protocol's policy-based authorization system
+ * @dev Leverages Predicate Protocol's policy-based authorization system to validate user permissions
  */
-contract PredicatePermitter is PredicateClient, Ownable, IPermitter {
+contract PredicatePermitter is
+	PredicateClient,
+	UUPSUpgradeable,
+	OwnableUpgradeable,
+	IPermitter
+{
 	/**
 	 * @notice Error thrown when an address parameter is the zero address
-	 * @dev Used in constructor to validate admin and predicateManager addresses
+	 * @dev Used in initialize function to validate admin and predicateManager addresses
 	 */
 	error AddressZero();
 
 	/**
 	 * @notice Error thrown when the policy ID string is empty
-	 * @dev Used in constructor to validate the policyID parameter
+	 * @dev Used in initialize function to validate the policyID parameter
 	 */
 	error PolicyIDEmpty();
 
 	/**
 	 * @notice Emitted when the Predicate policy ID is set or updated
-	 * @dev Triggered in constructor and setPolicy functions
+	 * @dev Triggered in initialize and setPolicy functions
 	 * @param policyID The new policy ID that was set
 	 */
 	event PolicySet(string policyID);
 
 	/**
 	 * @notice Emitted when the Predicate manager address is set or updated
-	 * @dev Triggered in constructor and setPredicateManager functions
+	 * @dev Triggered in initialize and setPredicateManager functions
 	 * @param predicateManager The new Predicate manager address
 	 */
 	event PredicateManagerSet(address predicateManager);
 
+	/// @custom:oz-upgrades-unsafe-allow constructor
+	constructor() {
+		_disableInitializers();
+	}
+
 	/**
-	 * @notice Creates a new PredicatePermitter contract
+	 * @notice Initializes the PredicatePermitter contract
 	 * @dev Sets up the initial state with admin, Predicate manager, and policy ID
 	 * @param _admin Address that will be granted ownership of the contract
 	 * @param _predicateManager Address of the Predicate Protocol manager contract
 	 * @param policyID The policy ID string used for permission validation
 	 */
-	constructor(
+	function initialize(
 		address _admin,
 		address _predicateManager,
-		string memory policyID
-	) Ownable(_admin) {
+		string calldata policyID
+	) external initializer {
 		if (_admin == address(0) || _predicateManager == address(0)) {
 			revert AddressZero();
 		}
 		if (bytes(policyID).length == 0) {
 			revert PolicyIDEmpty();
 		}
-
+		__Ownable_init(_admin);
+		__UUPSUpgradeable_init();
 		_initPredicateClient(_predicateManager, policyID);
 		emit PolicySet(policyID);
 		emit PredicateManagerSet(_predicateManager);
@@ -100,4 +113,13 @@ contract PredicatePermitter is PredicateClient, Ownable, IPermitter {
 		_setPredicateManager(serviceManager);
 		emit PredicateManagerSet(serviceManager);
 	}
+
+	/**
+	 * @notice Authorizes an upgrade to a new implementation
+	 * @dev Can only be called by the contract owner
+	 * @param newImplementation Address of the new implementation contract
+	 */
+	function _authorizeUpgrade(
+		address newImplementation
+	) internal override onlyOwner {}
 }
